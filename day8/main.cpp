876b883a4ad8f0058710f0fbd83acc062e9b90e0
rh <rabiahozar@gmail.com>
2024-12-08 13:53:12 +0100

    DRY Day8

--- day8/main.cpp
+++ day8/main.cpp
@@ -1,25 +1,36 @@
 #include <fstream>
 #include <iostream>
 #include <regex>
-#include <set>
 #include <sstream>
 #include <string>
+#include <unordered_set>
 #include <vector>
 
+// Define custom hash function for std::pair
+struct pair_hash
+{
+    template <class T1, class T2>
+    std::size_t operator()(const std::pair<T1, T2>& p) const
+    {
+        auto hash1 = std::hash<T1>{}(p.first);
+        auto hash2 = std::hash<T2>{}(p.second);
+        // Combine hashes using XOR and bit shifting
+        return hash1 ^ (hash2 << 1);
+    }
+};
+using AntinodeSet = std::unordered_set<std::pair<int64_t, int64_t>, pair_hash>;
 bool g_verbose = false;
+
 bool hasAntenna(const std::vector<std::vector<char>>& map, size_t x, size_t y,
                 char type)
 {
-    if (x >= 0 && y >= 0 && x < map.size() && !map.empty() &&
-        y < map[0].size() && map[x][y] == type)
-        return true;
-
-    return false;
+    return x < map.size() && !map.empty() && y < map[0].size() &&
+           map[x][y] == type;
 }
 
 bool addAntinode(const std::vector<std::vector<char>>& map, int64_t x,
                  int64_t y, int64_t distanceX, int64_t distanceY,
-                 std::set<std::pair<int64_t, int64_t>>& antinodes)
+                 AntinodeSet& antinodes)
 {
     if (x - distanceX >= 0 && x - distanceX < map.size() &&
         y - distanceY >= 0 && y - distanceY < map[0].size())
@@ -33,9 +44,9 @@ bool addAntinode(const std::vector<std::vector<char>>& map, int64_t x,
     return false;
 }
 
-void findAntinodes(const std::vector<std::vector<char>>& map)
+void processAntennas(const std::vector<std::vector<char>>& map,
+                     AntinodeSet& antinodes, bool calibrate)
 {
-    std::set<std::pair<int64_t, int64_t>> antinodes;
     for (size_t x = 0; x < map.size(); x++)
     {
         for (size_t y = 0; y < map[0].size(); y++)
@@ -54,88 +65,64 @@ void findAntinodes(const std::vector<std::vector<char>>& map)
                             if (g_verbose)
                                 std::cout << "Another Antenna  [" << i << " , "
                                           << j << " ]" << std::endl;
-                            int distanceX = x - i;  // -1
-                            int distanceY = y - j;  // 3
-                            addAntinode(map, x, y, distanceX * (-1),
-                                        distanceY * (-1), antinodes);
-                            addAntinode(map, i, j, distanceX, distanceY,
-                                        antinodes);
+                            int distanceX = x - i;
+                            int distanceY = y - j;
+
+                            if (calibrate)
+                            {
+                                int64_t referenceX = x, referenceY = y;
+                                while (addAntinode(map, referenceX, referenceY,
+                                                   distanceX * -1,
+                                                   distanceY * -1, antinodes))
+                                {
+                                    referenceX -= distanceX * -1;
+                                    referenceY -= distanceY * -1;
+                                }
+                                referenceX = i;
+                                referenceY = j;
+                                while (addAntinode(map, referenceX, referenceY,
+                                                   distanceX, distanceY,
+                                                   antinodes))
+                                {
+                                    referenceX -= distanceX;
+                                    referenceY -= distanceY;
+                                }
+                                antinodes.emplace(x, y);
+                                antinodes.emplace(i, j);
+                            }
+                            else
+                            {
+                                addAntinode(map, x, y, distanceX * -1,
+                                            distanceY * -1, antinodes);
+                                addAntinode(map, i, j, distanceX, distanceY,
+                                            antinodes);
+                            }
                         }
                     }
                 }
             }
         }
     }
+}
 
+void findAntinodes(const std::vector<std::vector<char>>& map)
+{
+    AntinodeSet antinodes;
+    processAntennas(map, antinodes, false);
     std::cout << "Result 1: " << antinodes.size() << std::endl;
 }
 
 void findAntinodesWithCalibration(const std::vector<std::vector<char>>& map)
 {
-    std::set<std::pair<int64_t, int64_t>> antinodes;
-    for (size_t x = 0; x < map.size(); x++)
-    {
-        for (size_t y = 0; y < map[0].size(); y++)
-        {
-            if (map[x][y] != '.')
-            {
-                std::cout << "Antenna  [" << x << " , " << y
-                          << " ] : " << map[x][y] << std::endl;
-                for (size_t i = x + 1; i < map.size(); i++)
-                {
-                    for (size_t j = 0; j < map[0].size(); j++)
-                    {
-                        if (hasAntenna(map, i, j, map[x][y]))
-                        {
-                            std::cout << "Another Antenna  [" << i << " , " << j
-                                      << " ]" << std::endl;
-                            int distanceX = x - i;  // -1
-                            int distanceY = y - j;  // 3
-
-                            int64_t referenceX = x;
-                            int64_t referenceY = y;
-                            while (addAntinode(map, referenceX, referenceY,
-                                               distanceX * (-1),
-                                               distanceY * (-1), antinodes))
-                            {
-                                referenceX -= distanceX * (-1);
-                                referenceY -= distanceY * (-1);
-                                                        }
-
-                            referenceX = i;
-                            referenceY = j;
-                            while (addAntinode(map, referenceX, referenceY,
-                                               distanceX, distanceY, antinodes))
-                            {
-                                referenceX -= distanceX;
-                                referenceY -= distanceY;
-                            }
-                            antinodes.emplace(x, y);
-                            antinodes.emplace(i, j);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
+    AntinodeSet antinodes;
+    processAntennas(map, antinodes, true);
     std::cout << "Result 2: " << antinodes.size() << std::endl;
 }
 
 int main(int argc, char** argv)
 {
-    bool mini_input = false;
-
-    if (argc >= 2 && std::string(argv[1]) == "mini")
-    {
-        mini_input = true;
-    }
-    bool verbose = false;
-    if (argc >= 3 && std::string(argv[2]) == "verbose")
-    {
-        verbose = true;
-        g_verbose = verbose;
-    }
+    bool mini_input = argc >= 2 && std::string(argv[1]) == "mini";
+    g_verbose = argc >= 3 && std::string(argv[2]) == "verbose";
 
     std::string fileName = mini_input ? "../mini_input" : "../input";
     std::ifstream file(fileName);
@@ -148,27 +135,24 @@ int main(int argc, char** argv)
 
     std::string line;
     std::vector<std::vector<char>> map;
-    if (verbose)
+    if (g_verbose)
         std::cout << 0;
     while (std::getline(file, line))
     {
-        if (verbose)
+        if (g_verbose)
         {
             if (map.empty())
             {
-                for (size_t i = 0; i < line.size(); i++)
-                {
-                    std::cout << i % 10;
-                }
+                for (size_t i = 0; i < line.size(); i++) std::cout << i % 10;
                 std::cout << std::endl;
             }
-            std::cout << (map.size()) % 10;
-            std::cout << line << std::endl;
+            std::cout << map.size() % 10 << line << std::endl;
         }
         map.push_back({line.begin(), line.end()});
     }
 
-    // findAntinodes(map);
+    findAntinodes(map);
+
     findAntinodesWithCalibration(map);
 
     return 0;
